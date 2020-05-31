40b9d21861d77aadbae55e51b53a9afd60afd493
alejandro1403 <65137750+alejandro1403@users.noreply.github.com>
2020-05-31 18:14:56 -0500

    Update uptime.c

--- uptime.c
+++ uptime.c
@@ -4,6 +4,18 @@
 int
 main(int argc, char *argv[])
 {
- printf(1, "up %d clock ticks\n", uptime());
+ if(argc > 1)
+ {
+   int cc = atoi(argv[1]); // ciclos de reloj
+   int time = (60*uptime())/cc;
+   int horas = time / 3600;
+   int min = (time-horas*3600)/60;
+   int sec = time-(horas*3600+min*60);
+   printf(1, "Up %d hours %d minutes %d seconds\n", horas, min, sec);
+ }
+ else
+ {
+   printf(2, "falta colocar los ciclos de reloj\n");
+ }
  exit();
 }
