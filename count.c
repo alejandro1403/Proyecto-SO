c604c2303adf60cfa55f8da02aab7bdfe4c2e53a
alejandro1403 <65137750+alejandro1403@users.noreply.github.com>
2020-05-31 20:19:21 -0500

    Update count.c

--- count.c
+++ count.c
@@ -1,1 +1,23 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+ int nsyscall = -1;
+ if (argc == 2) {
+  nsyscall = atoi(argv[1]);
+ }
+ if (nsyscall == -1) {
+  // mostrar todas las llamadas al sistema
+  // con su correspondiente numero de invocaciones
+   count(0);
+ } else {
+  // mostrar la llamada al sistema dada en nsyscall
+  // con su correspondiente numero de invocaciones
+   count(nsyscall);
+ }
+ exit();
+}
 
